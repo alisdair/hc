cb299406c38d6f86f7e0dea91365660dc759be9c
Alisdair McDiarmid & Jakub Konka <alisdair+pairingwith+jakub@randomoracle.com>
2014-02-09 21:21:18 +0000

    Use a type to represent operators.
    Remove evaluate accordingly.

--- Main.hs
+++ Main.hs
@@ -3,26 +3,16 @@ module Main where
 import System.Environment (getArgs)
 import Control.Monad (liftM)
 
-data Function = Multiply | Divide | Add | Subtract
-              deriving (Show,Eq)
+type Function = (Int -> Int -> Int)
 
 data Term = Operator Function | Number Int
-          deriving (Show,Eq)
 
 function :: String -> Maybe Function
-function "*" = Just Multiply
-function "/" = Just Divide
-function "+" = Just Add
-function "-" = Just Subtract
+function "*" = Just (*)
+function "/" = Just (div)
+function "+" = Just (+)
+function "-" = Just (-)
 function _   = Nothing
-
-evaluate :: Function -> Int -> Int -> Int
-evaluate op =
-    case op of
-      Multiply -> (*)
-      Divide   -> (div)
-      Add      -> (+)
-      Subtract -> (-)
 
 parse :: [String] -> [Term]
 parse = map go
@@ -32,7 +22,7 @@ parse = map go
             Nothing -> Number $ read x
 
 reduce :: [Term] -> [Int] -> Int
-reduce (Operator x:xs) (a:b:cs) = reduce xs $ evaluate x a b : cs
+reduce (Operator x:xs) (a:b:cs) = reduce xs $ x a b : cs
 reduce (Number x:xs) stack      = reduce xs $ x : stack
 reduce [] [x]                   = x
 reduce (Operator x:xs) _        = error "Stack underflow"
