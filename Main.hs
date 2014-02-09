c5e0ee415be26e1a25831810c9cd2ec358d6f464
Alisdair McDiarmid & Jakub Konka <alisdair+pairingwith+jakub@randomoracle.com>
2014-02-09 20:46:12 +0000

    Initial working implementation, almost.
    Does not report errors correctly.

--- Main.hs
+++ Main.hs
@@ -12,7 +12,7 @@ operator "+" = Just Add
 operator "-" = Just Subtract
 operator _   = Nothing
 
-evaluate :: Operator -> (Int -> Int -> Int)
+evaluate :: Operator -> Int -> Int -> Int
 evaluate op =
     case op of
       Multiply -> (*)
@@ -27,7 +27,14 @@ parse = map go
             Just y  -> Left y
             Nothing -> Right $ read x
 
+reduce :: [Either Operator Int] -> [Int] -> [Int]
+reduce (x:xs) stack =
+    case x of
+      Left y -> reduce xs $ evaluate y (head stack) (head $ tail stack) : drop 2 stack
+      Right y -> reduce xs $ y : stack
+reduce [] stack = stack
+
 main :: IO ()
 main = do
     args <- getArgs
-    print $ parse args
+    print $ reduce (parse args) []
